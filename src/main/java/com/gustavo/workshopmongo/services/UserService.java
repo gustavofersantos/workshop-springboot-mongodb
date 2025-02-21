a271cf1eb15b15c32fbe36df49b8a045f35eb93d
Gustavo Fernandes <gustavofdev@gmail.com>
2025-02-21 16:17:36 -0300

    feature: user findById and Exceptions

--- src/main/java/com/gustavo/workshopmongo/services/UserService.java
+++ src/main/java/com/gustavo/workshopmongo/services/UserService.java
@@ -1,20 +1,29 @@
 package com.gustavo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.gustavo.workshopmongo.domain.User;
 import com.gustavo.workshopmongo.repository.UserRepository;
+import com.gustavo.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepository;
 	
 	public List<User> findAll() {
-		return repo.findAll();
+		return userRepository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 }
