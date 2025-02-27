43443f2c1195b6d1fed9a0b4bf76ab3b8a55a927
Gustavo Fernandes <gustavofdev@gmail.com>
2025-02-27 14:31:59 -0300

    feature: getting posts by id

---
+++ src/main/java/com/gustavo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.gustavo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavo.workshopmongo.domain.Post;
+import com.gustavo.workshopmongo.repository.PostRepository;
+import com.gustavo.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+}
